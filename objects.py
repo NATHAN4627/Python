5fffc5a5f2bb9ba6a700e80928ed74d8da7997c1
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-12 17:58:49 +0900

    feat: learn python Objects

---
+++ objects.py
@@ -0,0 +1,11 @@
+import math
+from math import ceil
+
+print("===Objects===")
+# Object has state and method properties
+
+res = math.ceil(95.2)
+print("res = ",res)
+
+res2 = ceil(93.8)
+print("res2 = ",res2)
