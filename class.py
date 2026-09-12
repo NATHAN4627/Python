b8f38802eec211780f2bb0cd184268af25dcc601
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-13 01:52:42 +0900

    feat: learn special methods

--- class.py
+++ class.py
@@ -14,19 +14,47 @@ class Person():
         print(f"I am {self.age} years old!")
 
     @classmethod
-    #@staticmethod => if (cls) no need
+    # @staticmethod => if (cls) no need
     def about(cls):
         print("static method property executed!")
 
 
 # ordinary state
-person = Person("Kevin", 21)
-person.introduce()
-person.sayAge()
+# person = Person("Kevin", 21)
+# person.introduce()
+# person.sayAge()
 
 
-# static state
-print(Person.message)
+# # static state
+# print(Person.message)
 
-#static method
-Person.about()
+# #static method
+# Person.about()
+
+
+print("===special methods===")
+
+
+class Car():
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    def startEngine(self):
+        print(f"{self.name} started engine")
+
+    def stopEngine(self):
+        print(f"{self.name} stopped engine")
+
+    def __str__(self):
+        return f"Car name {self.name}, produced in {self.year}"
+
+    def __call__(self):
+        print("Object method called")
+
+
+car = Car("Urus", 2025)
+car.startEngine()
+car.stopEngine()
+print(car)
+car()
