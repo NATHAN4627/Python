6ad82a7af3e823ae121e09996e8b5115d52103c8
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-14 08:46:04 +0900

    F Task Executed

--- train.js
+++ train.js
@@ -1,7 +1,15 @@
-//G Task
-function getHighestIndex(arr) {
-    const maxNumber = Math.max(...arr);
-    return arr.indexOf(maxNumber);
+// G Task
+function getHighestIndex(number) {
+  const res = Math.max(...number);
+  return number.indexOf(res);
 }
 
-console.log(getHighestIndex([1, 4, 7, 13]));
+console.log(getHighestIndex([2, 5, 1, 7]));
+
+// //F Task
+// function getHighestIndex(arr) {
+//     const maxNumber = Math.max(...arr);
+//     return arr.indexOf(maxNumber);
+// }
+
+// console.log(getHighestIndex([1, 4, 7, 13]));
