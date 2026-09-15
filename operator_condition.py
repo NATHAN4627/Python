c1d60fc403c6828d1980295f1f7999b625980ccd
NATHAN4627 <okilovnodir456@gmail.com>
2026-09-15 13:48:36 +0900

    feat: learn conditions

--- operator_condition.py
+++ operator_condition.py
@@ -1,6 +1,6 @@
 print("===Operators===")
 # + -> >= <= % += -= ** //
-# is
+# is not or and
 
 a = 18
 b = 4
@@ -17,3 +17,33 @@ d = dict(name="Nathan", age=21)
 
 print(c == d)  # compares only value not references
 print(c is d)
+
+
+print("===Conditions===")
+# x = 90
+
+# if x > 50:
+#     print("case A")
+# elif x > 10:
+#     print("case B")
+# else:
+#     print("case C")
+
+
+age = 10
+
+# Ternary operator (in Python there is no condition ? a : b)
+person = "adult" if age > 18 else "child"
+print(person)
+
+
+
+is_student = False
+is_admin = True
+
+if not is_student:
+    print("You are not student!")
+elif is_admin:
+    print("You are admin!")
+else:
+   print("else case")
